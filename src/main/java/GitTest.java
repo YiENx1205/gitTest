36136658f55d1c6f420904179172b1a1dc0914d5
YiENx <yienx1205@gmail.com>
2022-04-25 21:03:34 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Date 2022/4/25
+ * @Author YiENx
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
